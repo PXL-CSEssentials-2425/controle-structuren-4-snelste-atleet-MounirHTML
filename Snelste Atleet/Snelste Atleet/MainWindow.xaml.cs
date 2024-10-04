86c6002c92f5d78dbbde8e9b5ff5f2e5226cbb51
Mounirelouahabipxl <11802826@student.pxl.be>
2024-10-04 12:29:18 +0200

    Done, nakijken er is een probleem.

--- Snelste Atleet/Snelste Atleet/MainWindow.xaml.cs
+++ Snelste Atleet/Snelste Atleet/MainWindow.xaml.cs
@@ -16,9 +16,62 @@ namespace Snelste_Atleet
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string nameFastest;
+        int secondFastest = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            int secondsCurrent;
+            bool isValidNumber = int.TryParse(textBoxSec.Text, out secondsCurrent);
+
+            if (isValidNumber == true)
+            {
+                if (secondsCurrent < secondFastest)
+                {
+                    secondFastest = secondsCurrent;
+                    nameFastest = textBoxNaam.Text;
+                }
+
+            }
+        }
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Resultaat.Clear();
+            textBoxNaam.Clear();
+            textBoxSec.Clear();
+
+            textBoxNaam.Focus();
+        }
+
+        private void atleetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Resultaat.Text = $"De snelste atleet is {nameFastest} met een tijd van {secondFastest} seconden.";
+        }
+
     }
 }
+
+            
+        
+                
+
+               
+            
+        
